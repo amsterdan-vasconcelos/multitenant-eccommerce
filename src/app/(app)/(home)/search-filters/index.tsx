115d8088abc503570b12be892820456188c7c8c8
Amsterdan Vasconcelos <amsterdan.email@gmail.com>
2025-10-13 11:22:41 -0300

    2 teste pre-commit

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -6,7 +6,6 @@ export function SearchFilters({ data }: { data: any }) {
     <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
       <SearchInput />
       <Categories data={data} />
-      <div></div>
     </div>
   );
 }
